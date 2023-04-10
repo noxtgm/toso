67cb6a95c00ca11a63ac572d9dfd7971c756cd5d
NoxTGM <noxtgm@gmail.com>
2023-04-10 11:38:10 +0200

    Update toso.py so it automatically runs as admin

--- toso.py
+++ toso.py
@@ -1,39 +1,56 @@
-import os
-import shutil
+import ctypes, os, sys, shutil
 
-# Flush the DNS Resolver Cache
-os.system("ipconfig /flushdns")
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except AttributeError:
+        raise RuntimeError("Admin check not supported on this platform.")
 
-# Scan for and repairs corrupt system files (requires admin privileges)
-os.system("sfc /scannow")
+def main():
+    # Folder paths that will get their content deleted
+    folder_paths = [
+        "C:/Users/noxtg/Pictures/Screenshots", # Cleans the screenshots made using the WIN + SHIFT + S shortcut
+        "C:/Users/noxtg/AppData/Local/Temp", # Cleans the temporary files created by applications 
+        "C:/Users/noxtg/AppData/Local/CrashDumps" # Cleans the applications' crash dumps
+    ]
 
-# Check for errors on the hard drive (requires admin privileges)
-os.system("chkdsk")
+    for folder_path in folder_paths:
+        # Check if the folder exists
+        if os.path.exists(folder_path):
+            # Get the contents of the folder
+            folder_contents = os.listdir(folder_path)
+            # Loop through each item in the folder
+            for item in folder_contents:
+                # Get the full path of the item
+                item_path = os.path.join(folder_path, item)
+                try:
+                    # If the item is a file, delete it
+                    if os.path.isfile(item_path):
+                        os.remove(item_path)
+                        print(f"Successfully deleted file: {item_path}")
+                    # If the item is a directory, delete it recursively
+                    elif os.path.isdir(item_path):
+                        shutil.rmtree(item_path)
+                        print(f"Successfully deleted directory: {item_path}")
+                # Catch any OSError exceptions that occur during the deletion process
+                except OSError as e:
+                    print(f"Error deleting file: {item_path}. {e}")
+        # Print an error message if the folder does not exist
+        else:
+            print(f"Folder does not exist: {folder_path}")
+    
+    # Flush the DNS Resolver Cache
+    os.system("ipconfig /flushdns")
 
-# Folder paths that will get their content deleted
-folder_paths = "/Users/user/AppData/Local/Temp", "/Users/user/Pictures/Screenshots", "/Users/user/AppData/Local/CrashDumps", "/$Recycle.Bin/S-1-5-21-4083530894-3095089788-2986954845-1003"
+    # Scan for and repairs corrupt system files (requires admin privileges)
+    os.system("sfc /scannow")
 
-for folder_path in folder_paths:
-    # Check if the folder exists
-    if os.path.exists(folder_path):
-        # Get the contents of the folder
-        folder_contents = os.listdir(folder_path)
-        # Loop through each item in the folder
-        for item in folder_contents:
-            # Get the full path of the item
-            item_path = os.path.join(folder_path, item)
-            try:
-                # If the item is a file, delete it
-                if os.path.isfile(item_path):
-                    os.remove(item_path)
-                    print(f"Successfully deleted file: {item_path}")
-                # If the item is a directory, delete it recursively
-                elif os.path.isdir(item_path):
-                    shutil.rmtree(item_path)
-                    print(f"Successfully deleted directory: {item_path}")
-            # Catch any OSError exceptions that occur during the deletion process
-            except OSError as e:
-                print(f"Error deleting file: {item_path}. {e}")
-    # Print an error message if the folder does not exist
+    # Check for errors on the hard drive (requires admin privileges)
+    os.system("chkdsk")
+
+if __name__ == "__main__":
+    if is_admin():
+        main()
     else:
-        print(f"Folder does not exist: {folder_path}")
+        # Re-run the script with admin privileges
+        ctypes.windll.shell32.ShellExecuteW( None, "runas", sys.executable, f'"{__file__}"', None, 1 )
